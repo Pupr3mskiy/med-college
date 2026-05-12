b6fdc1ef5ba38a0f4ad596c52cc5cfd8b25d62a4
NarcoSova <97403073+NarcoSova@users.noreply.github.com>
2026-05-12 14:25:19 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -6,59 +6,5 @@ const jwt = require('jsonwebtoken');
 
 const users = [];
 
-// REGISTER
-router.post('/register', async (req, res) => {
-
-    const { email, password } = req.body;
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = {
-        email,
-        password: hashedPassword
-    };
-
-    users.push(user);
-
-    res.json({
-        message: 'User registered',
-        users
-    });
-});
-
-// LOGIN
-router.post('/login', async (req, res) => {
-
-    const { email, password } = req.body;
-
-    const user = users.find(u => u.email === email);
-
-    if (!user) {
-        return res.status(404).json({
-            message: 'User not found'
-        });
-    }
-
-    const isPasswordCorrect = await bcrypt.compare(
-        password,
-        user.password
-    );
-
-    if (!isPasswordCorrect) {
-        return res.status(401).json({
-            message: 'Wrong password'
-        });
-    }
-
-    const token = jwt.sign(
-        { email: user.email },
-        'SECRET_KEY'
-    );
-
-    res.json({
-        message: 'Login success',
-        token
-    });
-});
 
 module.exports = router;
