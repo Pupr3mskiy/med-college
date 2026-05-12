e16719fb6838d5a68cf31693b6ea8e8d39f4a4ba
NarcoSova <97403073+NarcoSova@users.noreply.github.com>
2026-05-12 11:06:00 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.use(express.json());
-
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.listen(3000, () => {
-  console.log('Server running');
-});
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+app.listen(3000, () => console.log('Server running'))
