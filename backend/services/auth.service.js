a80cde5911a3f85401d8a9d9d23297129831f6d3
NarcoSova <97403073+NarcoSova@users.noreply.github.com>
2026-05-20 14:37:32 +0300

    1

--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 
 const users = [];
 
+///РЕГИСТРАЦИЯ
 const registerUser = async (user_name,email,password) =>{
     const hashedPassword = await bcrypt.hash(password,10);
 
@@ -19,3 +20,35 @@ const registerUser = async (user_name,email,password) =>{
         email
     };
 };
+
+///ЛОГИН
+const loginUser = async (email,password) => {
+
+    const user = users.find(u => u.email ===email);
+
+    if (!user) {throw new Error ('User not found');}
+
+    const isPasswordCorrect = await bcrypt.compare(password,user.password);
+
+    if (!isPasswordCorrect) {throw new Error("Wrong password");}
+
+
+    const token = jwt.sign(
+        {email:user.email},
+        'SECRET_KEY',
+        {expiresIn:'1h'}
+    );
+
+
+
+    return {
+        token,user:{user_name:user.user_name,
+            email:user.email
+        }
+    };
+};
+
+module.exports = {
+    registerUser,
+    loginUser
+}
